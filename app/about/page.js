838a19864ab75cda3f7ca7c315fd001be2ff968a
SimjotKaur <141382281+SimjotKaur@users.noreply.github.com>
2024-08-08 12:16:51 -0600

    about page

--- app/about/page.js
+++ app/about/page.js
@@ -12,7 +12,9 @@ export default function About() {
             <h1 className="text-2xl font-bold mb-2">Getting to Know</h1>
             <h2 className="text-3xl font-extrabold mb-2">Calvin Dang</h2>
             <p className="text-base mb-4 text-center">
-              I am an interior designer with a passion for transforming spaces into beautiful, functional environments. With a keen eye for detail and a deep understanding of design principles, I strive to create spaces that reflect the unique tastes and needs of my clients. My work includes residential and commercial projects, where I focus on combining aesthetics with practicality. Through collaboration and creativity, I aim to deliver designs that inspire and elevate everyday living.
+              I am an interior designer with a passion for transforming spaces into beautiful, functional environments.
+              With a keen eye for detail and a deep understanding of design principles, I strive to create spaces that reflect the unique tastes and needs of my clients.
+              My work includes residential and commercial projects, where I focus on combining aesthetics with practicality. Through collaboration and creativity, I aim to deliver designs that inspire and elevate everyday living.
             </p>
             <Link href="/contact" legacyBehavior>
               <a className="mt-2 px-3 py-2 bg-black text-white rounded hover:bg-black">
